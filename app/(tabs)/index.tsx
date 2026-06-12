09d92d18e6f720f0d429af57f9cc8c3c5dd9896a
Sneha Shaw <sshaw@gbtechservice.com>
2026-06-12 14:34:13 +0530

    fixed package issues and added assets

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,8 +3,8 @@ import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <SafeAreaView className="flex-1 items-center justify-center bg-slate-400">
-      <Text>Index</Text>
+    <SafeAreaView className="flex-1 justify-center bg-white">
+      
     </SafeAreaView>
   );
 }
--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,7 @@
 const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require('nativewind/metro');
- 
-const config = getDefaultConfig(__dirname)
- 
-module.exports = withNativeWind(config, { input: './global.css' })
+const { withNativewind } = require("nativewind/metro");
+
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = getDefaultConfig(__dirname);
+
+module.exports = withNativewind(config);
